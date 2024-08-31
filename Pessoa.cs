bc8e1b5146ba12f130774fc297dbd27cff2a36c4
Dan <dansantosalfa@gmail.com>
2024-08-31 16:15:31 -0300

    feat: create Pessoa e Suite

---
+++ Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Sarigue
+{
+    public class Pessoa
+    {
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+
+        public string Nome { get; set; } = string.Empty;
+        public string Sobrenome { get; set; } = string.Empty;
+    }
+}
